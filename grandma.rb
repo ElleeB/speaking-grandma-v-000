6abfbc28d0fcf65555f4f5ba7b747419675561b2
Laura Trager <laura.trager@gmail.com>
2017-10-26 17:57:42 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,5 +1,5 @@
 def speak_to_grandma(phrase)
-  if phrase == phrase.upcase && prase != "I LOVE YOU GRANDMA!"
+  if phrase == phrase.upcase && phrase != "I LOVE YOU GRANDMA!"
     return "NO, NOT SINCE 1938!"
   elsif phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
